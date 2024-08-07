542125599df67168bd07a09d84c358c31313eb87
isimsek7 <isimsekmusic@gmail.com>
2024-08-07 21:10:40 +0300

    Add files via upload

---
+++ EmptyClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace ClassObj
+{
+	public class Bebek
+	{
+		public Bebek()
+		{
+            Console.WriteLine("Ingaa!");
+            DateTime tarih = DateTime.Now;
+            _dogumTarihi = tarih.ToLongDateString();
+        }
+		public Bebek(string ad, string soyad)
+		{
+			Console.WriteLine("Ingaa!");
+			_ad = ad;
+			_soyAd = soyad;
+			_dogumTarihi = DateTime.Now.ToLongDateString();
+			Console.WriteLine("{0} tarihinde, {1} {2}, isimli bir bebek dunyaya geldi.", _dogumTarihi, _ad, _soyAd);
+		}
+
+		public string _ad { get; set; }
+		public string _soyAd { get; set; }
+		public string _dogumTarihi{ get; set; }
+
+
+		public void ozet()
+		{
+            _dogumTarihi = DateTime.Now.ToLongDateString();
+			Console.WriteLine("{0} tarihinde, {1} {2}, isimli bir bebek dunyaya geldi.", _dogumTarihi, _ad, _soyAd);
+            
+        }
+	}
+	
+}
+
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using ClassObj;
+string x = "Murtaza";
+string y = "Gundogdu";
+Bebek bebek1 = new Bebek(x, y);
+Bebek bebek2 = new Bebek();
+bebek2._ad = "Selin";
+bebek2._soyAd = "Candar";
+bebek2.ozet();
